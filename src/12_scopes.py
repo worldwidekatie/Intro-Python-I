ead1706001de0bdecb24cae11e48cb76386f4fa6
repl.it user <replituser@example.com>
2020-08-03 20:02:20 +0000

    12

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -6,10 +6,12 @@ x = 12
 
 def change_x():
     x = 99
+    return x
 
-change_x()
+x = change_x()
 
-# This prints 12. What do we have to modify in change_x() to get it to print 99?
+# This prints 12. What do we have to modify in change_x() to get it to print 99? We have to set it to a 
+# local varialbe of and change the function to return x
 print(x)
 
 
@@ -20,11 +22,12 @@ def outer():
 
     def inner():
         y = 999
+        return y
 
-    inner()
+    y = inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
-    # 999?
+    # 999? return y and y = inner()
     # Note: Google "python nested function scope".
     print(y)
 
