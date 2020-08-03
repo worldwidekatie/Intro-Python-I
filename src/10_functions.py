4f215bc7a8ffe4b46386531072d4bf8b047ecb30
repl.it user <replituser@example.com>
2020-08-03 19:29:21 +0000

    09

--- src/10_functions.py
+++ src/10_functions.py
@@ -8,5 +8,11 @@ num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
-# YOUR CODE HERE
+def is_even(number):
+  if num%2 == 0:
+    print("Even!")
+  else:
+    print("Odd")
 
+
+is_even(num)
